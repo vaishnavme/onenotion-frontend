f39fb194ff93857da2c45353e0a6410b5d200fd0
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-25 18:58:46 +0530

    Navbar style update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,11 @@ export const Navbar = () => {
     const [toggle, setToggle] = useState(false);
     
     const activeColor = {
-        color: "#22D3EE"
+        color: "#000000",
+        backgroundColor: "#ffffff",
+        borderRadius: "0.4rem",
+        transition: "ease 200ms",
+        boxShadow: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)"
     }
     const toggleBtnHandler = () => setToggle(
         (prevState) => !prevState
@@ -13,10 +17,10 @@ export const Navbar = () => {
 
     return (
         <React.Fragment>
-            <header className="fixed top-0 left-0 w-full shadow bg-white px-4 z-10">
+            <header className="fixed top-0 left-0 w-full shadow bg-white px-4 z-10 md:hidden">
                 <div className="flex items-center justify-between h-16">
-                    <NavLink to="/" className="text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 md:ml-20">
-                        Trivia Fort
+                    <NavLink to="/" className="text-2xl md:ml-20 md:hidden">
+                        One Notion
                     </NavLink>
                     <div className="text-2xl cursor-pointer md:hidden" onClick={() => toggleBtnHandler()}>
                         <i className={toggle ? `bx bx-x` : `bx bx-menu-alt-right`}></i>
@@ -24,30 +28,38 @@ export const Navbar = () => {
                 </div>
             </header>
 
-            <div className={toggle ? `${navStyle} left-0` : `${navStyle} -left-full md:left-0 w-20 pt-5 px-6`}>
+            <div className={toggle ? `${navStyle} left-0 w-64` : `${navStyle} -left-full w-52 md:left-0 pt-5 px-6`}>
                 <nav className="flex justify-between flex-col h-full pb-12 overflow-auto">
                     <div>
-                        <NavLink to="/" className="mb-10 font-semibold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 flex items-center">
-                            <i className="bx bxs-disc text-2xl"></i>
-                            <span className="text-lg whitespace-nowrap font-medium ml-4">Trivia Fort</span>
+                        <NavLink to="/" className="mb-10 font-semibold  flex items-center">
+                            <i className='bx bx-wind text-2xl'></i>
+                            <span className="text-lg whitespace-nowrap ml-4">One Notion</span>
                         </NavLink>
 
-                        <div className="grid gap-y-10">
+                        <div className="grid gap-y-10 mt-32">
                             <div className="grid gap-y-6">
-                                <NavLink to="/" activeStyle={activeColor} className="flex items-center text-gray-600" end>
-                                    <i className="bx bx-home text-xl"></i>
-                                    <span className="text-base whitespace-nowrap font-medium ml-4">Home</span>
+                                <NavLink to="/create" activeStyle={activeColor} className="flex items-center text-gray-500 pt-1">
+                                    <i className="bx bx-pencil text-2xl p-1"></i>
+                                    <span className="text-base whitespace-nowrap ml-4">Create New</span>
                                 </NavLink>
-                                <NavLink to="/dashbord" activeStyle={activeColor} className="flex items-center text-gray-600">
-                                    <i className="bx bxs-dashboard text-xl"></i>
-                                    <span className="text-base whitespace-nowrap font-medium ml-4">Dashboard</span>
+                                <NavLink to="/" activeStyle={activeColor} className="flex items-center text-gray-500 p-1" end>
+                                    <i className="bx bx-notepad text-2xl p-1"></i>
+                                    <span className="text-base whitespace-nowrap ml-4">All Notes</span>
+                                </NavLink>
+                                <NavLink to="/shared" activeStyle={activeColor} className="flex items-center text-gray-500 p-1">
+                                    <i className='bx bx-intersect text-2xl p-1'></i>
+                                    <span className="text-base whitespace-nowrap ml-4">Shared</span>
+                                </NavLink>
+                                <NavLink to="/account" activeStyle={activeColor} className="flex items-center text-gray-500 p-1">
+                                    <i className="bx bx-user text-2xl p-1"></i>
+                                    <span className="text-base whitespace-nowrap ml-4">Account</span>
                                 </NavLink>
                             </div>
                         </div>
                     </div>
-                    <NavLink to="/" className="flex items-center">
-                        <i className="bx bx-log-out text-lg"></i>
-                        <span className="text-base whitespace-nowrap font-medium ml-4">Log Out</span>
+                    <NavLink to="/" className="flex items-center p-1 rounded-lg hover:text-black hover:bg-white hover:shadow-md">
+                        <i className="bx bx-log-out text-2xl p-1"></i>
+                        <span className="text-base whitespace-nowrap ml-4">Log Out</span>
                     </NavLink>
                 </nav>
             </div>
@@ -55,4 +67,4 @@ export const Navbar = () => {
     )
 }
 
-const navStyle = "fixed top-0 h-full pt-4 px-4 pb-0 shadow bg-white z-10 overflow-hidden transition-all duration-500 ease-in-out"
+const navStyle = "fixed top-0 h-full pt-4 px-4 pb-0 shadow bg-gray-100 z-10 overflow-hidden transition-all duration-500 ease-in-out"
