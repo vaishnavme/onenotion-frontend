19356b1e096848d787ef8a676b94e6b536d7e728
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-25 17:49:26 +0530

    page to show data

---
+++ src/features/notes/Notions.jsx
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux"
+
+export default function Notions() {
+    const allNotions = useSelector((state) => state.notion.notes)
+
+    return (
+        <div>
+            <h1 className="text-2xl">Your Notes</h1>
+            <ul>
+                {
+                    allNotions.map((note) => (
+                        <li key={note.id}>
+                            <div className="text-lg font-medium">{note.title}</div>
+                            <p>{note.article}</p>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
