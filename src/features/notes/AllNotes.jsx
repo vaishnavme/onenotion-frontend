3ed8acc6163f53b6d918067b66e6d0742070bb7a
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-26 11:44:25 +0530

    basic layout and style

--- src/features/notes/AllNotes.jsx
+++ src/features/notes/AllNotes.jsx
@@ -6,16 +6,20 @@ export default function AllNotes() {
     return (
         <div>
             <h1 className="text-2xl">Your Notes</h1>
-            <ul>
+            <div className="grid grid-flow-col grid-flow-row auto-fr gap-12 my-8">
                 {
                     allNotions.notes.map((note) => (
-                        <li key={note.id}>
-                            <div className="text-lg font-medium">{note.title}</div>
+                        <div 
+                            className="bg-gray-50 rounded-md p-4 hover:shadow-lg transition-all duration-300 ease"
+                            key={note.id}>
+                            <div className="text-lg font-medium mb-2">{note.title}</div>
+                            <span className="text-gray-600">{note.label}</span>
+                            <div className="border-b-2 my-2"></div>
                             <p>{note.article}</p>
-                        </li>
+                        </div>
                     ))
                 }
-            </ul>
+            </div>
         </div>
     )
 }
