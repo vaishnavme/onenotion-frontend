37ea3dea49e4bcfb6ba429985d874e8219189ce9
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-26 21:01:21 +0530

    Search box added

--- src/features/notes/AllNotes.jsx
+++ src/features/notes/AllNotes.jsx
@@ -5,7 +5,15 @@ export default function AllNotes() {
 
     return (
         <div>
-            <h1 className="text-2xl">Your Notes</h1>
+            <div className="md:flex md:justify-between md:items-center">
+                <h1 className="text-2xl">Your Notes</h1>
+                <div className="my-4 flex md:justify-between md:items-center m-auto">
+                    <input className="block w-10/12 md:w-96 p-3 bg-gray-700 mr-2 md:m-auto rounded md:rounded-l-md" type="text" placeholder="Search"/>
+                    <button className="flex items-center px-3 py-2 bg-gray-800 hover:bg-gray-700 rounded md:rounded-r-md text-white">
+                        <span className="hidden md:block p-1">Filter</span> <i className=" md:ml-2 text-xl bx bx-filter-alt"></i>
+                    </button>
+                </div>
+            </div>
             <div className="notesGrid my-4">
                 {
                     allNotions.notes.map((note) => (
