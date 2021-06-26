fd8c8e63ab88b7b8212e3171cbe96bafc2ebbe12
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-26 12:58:36 +0530

    refactor

--- src/features/newNote/NewNote.jsx
+++ src/features/newNote/NewNote.jsx
@@ -18,23 +18,23 @@ export default function NewNote() {
                 </div>
             </div>
             <div className="m-auto w-full max-w-3xl">
-            <div
-                className="p-2 text-3xl font-bold rounded bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent"
-                contentEditable="plaintext-only"
-                suppressContentEditableWarning={true}
-                placeholder="Untitled"
-                onInputCapture={(e) => setTitle(e.target.innerText)}>
-                    {title==="" ? "" : null}
+                <div
+                    className="p-2 text-3xl font-bold rounded bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent"
+                    contentEditable="plaintext-only"
+                    suppressContentEditableWarning={true}
+                    placeholder="Untitled"
+                    onInputCapture={(e) => setTitle(e.target.innerText)}>
+                        {title==="" ? "" : null}
+                </div>
+                <div
+                    className="my-2 p-2 bg-gray-50 rounded focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent"
+                    contentEditable="plaintext-only"
+                    suppressContentEditableWarning={true}
+                    placeholder="Write your thoughts..."
+                    onInputCapture={(e) => setArticle(e.target.innerText)}>
+                        {article==="" ? "" : null}
+                </div>
             </div>
-            <div
-                className="my-2 p-2 bg-gray-50 rounded focus:outline-none focus:ring-2 focus:ring-gray-300 focus:border-transparent"
-                contentEditable="plaintext-only"
-                suppressContentEditableWarning={true}
-                placeholder="Write your thoughts..."
-                onInputCapture={(e) => setArticle(e.target.innerText)}>
-                    {article==="" ? "" : null}
-            </div>
-        </div>
         </div>
     )
 }
