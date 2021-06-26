7412d5fe0d3b5f438b745c45c89781ae0cd8be3a
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-26 11:09:38 +0530

    basic create new note

--- src/features/newNote/NewNote.jsx
+++ src/features/newNote/NewNote.jsx
@@ -1,8 +1,19 @@
 export default function NewNote() {
 
     return (
-        <div>
-            <h1 className="text-2xl">Create New Notes</h1>
+        <div className="m-auto w-full max-w-3xl">
+            <input 
+                className="p-2 text-2xl font-semibold w-full rounded bg-gray-50"
+                type="text" 
+                placeholder="Title"
+            />
+            
+            <div
+                className="my-2 p-2 bg-gray-50 rounded"
+                contentEditable={true}
+                suppressContentEditableWarning={true}
+                placeholder="Write your thoughts..."
+            ></div>
         </div>
     )
 }
