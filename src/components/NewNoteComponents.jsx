a27e179e0dd565b4c93bec33848c03414f07f003
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-06-30 09:35:02 +0530

    all markdown features added

--- src/components/NewNoteComponents.jsx
+++ src/components/NewNoteComponents.jsx
@@ -2,6 +2,22 @@ import { Fragment } from "react";
 import TextareaAutosize from 'react-autosize-textarea';
 const marked = require("marked");
 
+marked.setOptions({
+    renderer: new marked.Renderer(),
+    highlight: function(code, lang) {
+      const hljs = require('highlight.js');
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
+    },
+    pedantic: false,
+    gfm: true,
+    breaks: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
+    xhtml: false
+  });
+
 export const EditorContainer = ({title, article, setTitle, setArticle}) => {
     return (
         <Fragment>
