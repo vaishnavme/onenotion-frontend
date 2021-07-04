2852321a52822836f4ab34629a81b9dcec41536d
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-07-04 16:06:05 +0530

    reducer rename

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSliceReducer from "../features/auth/authSlice";
-import noteSliceReducer from '../features/pages/pageSlice';
+import pageSliceReducer from "../features/pages/pageSlice";
 import sharedPageReducer from "../features/shared/sharedSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSliceReducer,
-    notion: noteSliceReducer,
-    shared: sharedPageReducer
+    page: pageSliceReducer,
+    share: sharedPageReducer
   },
 });
