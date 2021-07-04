11be87cee867c0b2b3e5a824c0c38fc8460e10d8
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-07-04 16:27:53 +0530

    sharedSlice extra reducer added and function rename

--- src/features/shared/sharedSlice.js
+++ src/features/shared/sharedSlice.js
@@ -2,48 +2,75 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { getPublicPage, pagePublish, deletePublish } from "../../services/shared.service";
 
 export const getSharedPages = createAsyncThunk(
-    "pages/getPublicPage",
+    "shared/getSharedPages",
     async() => {
-        const {data: {success, message, sharedPages}} = await getPublicPage();
-        console.log({success, message});
+        const sharedPages = await getPublicPage();
         return sharedPages
     }
 )
 
 export const sharePage = createAsyncThunk(
-    "pages/pagePublish",
+    "shared/sharePage",
     async(pageId) => {
-        const {data: {success, message}} = await pagePublish(pageId);
-        console.log({success, message});
+        const data = await pagePublish(pageId);
+        return data
     }
 )
 
 export const deleteSharedPage = createAsyncThunk(
-    "pages/pagePublish",
+    "shared/deleteSharedPage",
     async(pageId) => {
-        const {data: {success, message}} = await deletePublish(pageId);
-        console.log({success, message});
-        }
+        // eslint-disable-next-line
+        const data = await deletePublish(pageId);
+        return pageId
+    }
 )
 
 
 export const sharedSlice = createSlice({
     name: "shared",
     initialState: {
-        sharedPages: [],
-        sharedStatus: "idle"
+        sharedStatus: "idle",
+        publicPage: [],
     },
+    reducers: {},
     extraReducers: {
         [getSharedPages.pending]: (state) => {
             state.sharedStatus = "loading"
         },
         [getSharedPages.fulfilled]: (state, action) => {
-            state.sharedPages = (action.payload);
+            const { payload } = action;
+            state.publicPage = payload;
             state.sharedStatus = "pageLoaded"
         },
         [getSharedPages.rejected]: (state) => {
             state.sharedStatus = "error"
         },
+
+        [sharePage.pending]: (state) => {
+            state.sharedPages = "loading"
+        },
+        [sharePage.fulfilled]: (state, action) => {
+            state.publicPage.push(action.payload)
+            state.sharedStatus = "pageLoaded"
+        },
+        [sharePage.rejected]: (state) => {
+            state.sharedPages = "error"
+        },
+
+        [deleteSharedPage.pending]: (state) => {
+            state.sharedPages = "loading"
+        },
+        [deleteSharedPage.fulfilled]: (state, action) => {
+            const { payload } = action;
+            console.log(state.sharedPages)
+            state.publicPage.splice(
+                state.publicPage.findIndex((page) => page._id === payload),1)
+                state.sharedStatus = "pageLoaded"
+        },
+        [deleteSharedPage.rejected]: (state) => {
+            state.sharedPages = "error"
+        }
     }
 })
 
