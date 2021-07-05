b143c0449c95746ec33bc012b2d51cda56fe2397
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-07-05 09:32:40 +0530

    bug fix

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import axios from "axios";
 import { useParams } from "react-router";
 import { PreviewContainer } from "."
 
 export const Page = () => {
+    const { status, authUserToken } = useSelector((state) => state.auth);
     const [pageData, setPageData] = useState("")
     const { pageId } = useParams();
-    console.log(pageData)
 
     useEffect(() => {
+        if(status === "tokenReceived") {
+            axios.defaults.headers.common["Authorization"] = authUserToken;
+        }
         (async () => {
             try {
                 const { data } = await axios.get(`/public/shared/${pageId}`)
