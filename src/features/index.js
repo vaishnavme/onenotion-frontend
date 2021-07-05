6e99ed866e2616e590aee6231f5cee9e45f979bd
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-07-05 16:34:41 +0530

    export bugg fix

--- src/features/index.js
+++ src/features/index.js
@@ -1,6 +1,6 @@
 export { default as AllPages } from "./pages/AllPages";
 export { default as NewPage } from "./pages/NewPage";
-export { default as Page } from "./shared/Shared";
+export { default as Page } from "./pages/Page";
 export { default as Shared } from "./shared/Shared";
 export { default as Login } from "./auth/Login";
 export { default as SignUp } from "./auth/SignUp";
