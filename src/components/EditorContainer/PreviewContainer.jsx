fc8efd97f7ddd22368517269bee5a71d1888e9d8
Vaishnav Chandurkar <vaishnavchandurkar12@gmail.com>
2021-08-10 14:23:23 +0530

    Preview Container

--- src/components/EditorContainer/PreviewContainer.jsx
+++ src/components/EditorContainer/PreviewContainer.jsx
@@ -1,9 +1,25 @@
-import styles from "../css/Markdown.module.css";
+// import styles from "../css/Markdown.module.css";
+import MarkdownIt from 'markdown-it';
+
+
+const Markdown = MarkdownIt({
+    html: true,
+    linkify: true,
+    typographer: true,
+    breaks: true,
+    quotes: '“”‘’',
+    highlight: function(code, lang) {
+        const hljs = require('highlight.js');
+        const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+        return hljs.highlight(code, { language }).value;
+    },
+});
 
 export const PreviewContainer = ({title,content}) => {
     return (
-        <div className={`${styles.markdown}`}>
+        <div>
             <div className="text-3xl font-bold">{title}</div>
+            <div className="my-3" dangerouslySetInnerHTML = {{__html: Markdown.render(content)}}></div>
         </div>
     )
 }
